476d7da440d97d787b804d04fed92571a45655d0
NueL72 <paulsiaga72@gmail.com>
2025-03-15 22:19:19 +0300

    trial

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './index.html', // Path to your HTML files
+      './src/**/*.{js,ts,jsx,tsx}', // Path to your JS/TS files (if applicable)
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
